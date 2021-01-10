90b34c116414f1e524fc853cac11c541f80f350d
Flavio fernando da Silva <tchfernando@gmail.com>
2021-01-10 02:06:31 -0300

    Finish front-web

--- front-web/src/Orders/types.tsx
+++ front-web/src/Orders/types.tsx
@@ -7,7 +7,15 @@ export type Product = {
 }
 
 export type OrderLocationData = {
-  address: string,
-  latitude: number,
-  longitude: number,
+  address: string;
+  latitude: number;
+  longitude: number;
 }
+
+type ProductId = {
+  id: number;
+}
+
+export type OrderPayload = {
+  products: ProductId[];
+} & OrderLocationData;
