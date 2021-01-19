dd7059b8018ff7df21a7adc9a5688a1977fbf617
tchfer <tchfernando@gmail.com>
2021-01-19 00:29:27 -0300

    Routes added

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../Header';
+
+function Orders() {
+  return (
+    <>
+      <Header />
+      <View>
+        <Text>Listagem de pedidos</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Orders;
